fb0c8ea512d0cc5b3442e0af29222371e9d77fcc
Pedro Freitas <pedrinfreitas@gmail.com>
2020-04-10 01:48:27 -0300

    +menu retratil +pages logon e register

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './global-normalize.css';
+import './global-styles.css';
+
+import Routes from './routes';
+
+const App = () => <Routes />;
+
+export default App;

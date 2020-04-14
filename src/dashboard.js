67bf2abdc4175e41c29acec0607c8831de3ea58d
Patrick Cruz <patrickcruz@MacBook-Pro-de-Patrick.local>
2020-04-14 20:46:26 -0300

    REV02

--- src/dashboard.js
+++ src/dashboard.js
@@ -29,44 +29,48 @@ export default function dashboard() {
 
 <CardDeck>
 <Card>
-  <Card.Img className="top" src={ box } alt="Imagem Box" />
+  <Card-Header>
+  <Card.Img className="top" src={ box } alt="Imagem Box"  />
+  <Card.Title>Card title</Card.Title>
+  </Card-Header>
   <Card.Body>
-    <Card.Title>Card title</Card.Title>
     <Card.Text>
       Testing o que eu quiser! supporting text below as a natural lead-in to
       additional content. This content is a little bit longer.
     </Card.Text>
   </Card.Body>
   <Card.Footer>
-    <small className="text-muted">Last updated 3 mins ago</small>
-  <IoIosArrowDown />
+  <IoIosArrowDown className ="arrowDown" />
   </Card.Footer>
 </Card>
 <Card>
+<Card-Header>
   <Card.Img className="top" src={ box } alt="Imagem Box"  />
+  <Card.Title>Card title</Card.Title>
+  </Card-Header>
   <Card.Body>
-    <Card.Title>Card title</Card.Title>
     <Card.Text>
       This card has supporting text below as a natural lead-in to additional
       content.{' '}
     </Card.Text>
   </Card.Body>
   <Card.Footer>
-    <small className="text-muted">Last updated 3 mins ago</small>
+    <IoIosArrowDown className ="arrowDown" />
   </Card.Footer>
 </Card>
 <Card>
+<Card-Header>
   <Card.Img className="top" src={ box } alt="Imagem Box"  />
+  <Card.Title>Card title</Card.Title>
+  </Card-Header>
   <Card.Body>
-    <Card.Title>Card title</Card.Title>
     <Card.Text>
       This is a wider card with supporting text below as a natural lead-in to
-      additional content. This card has even longer content than the first to
-      show that equal height action.
+      additional content.
     </Card.Text>
   </Card.Body>
   <Card.Footer>
-    <small className="text-muted">Last updated 3 mins ago</small>
+    <IoIosArrowDown className ="arrowDown" />
   </Card.Footer>
 </Card>
 </CardDeck>

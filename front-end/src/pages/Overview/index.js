963284355461efa046b3720b38be7b780b0dcfef
adlermo <adlerorteney@gmail.com>
2020-04-21 11:34:38 -0300

    CREATED: overview companies

--- front-end/src/pages/Overview/index.js
+++ front-end/src/pages/Overview/index.js
@@ -1,19 +1,91 @@
-import React from "react";
+import React, { Component } from "react";
+
+import { IoIosPeople, IoIosArrowForward, IoIosStar, IoIosStarOutline } from "react-icons/io";
 
 import './styles.css'
 
-const Overview = () => <>
-    <section className='card-container'>
-        <div className='card-item'>Teste de Texto</div>
-        <div className='card-item'>Teste de Texto</div>
-        <div className='card-item'>Teste de Texto</div>
-        <div className='card-item'>Teste de Texto</div>
-        <div className='card-item'>Teste de Texto</div>
-        <div className='card-item'>Teste de Texto</div>
-        <div className='card-item'>Teste de Texto</div>
-        <div className='card-item'>Teste de Texto</div>
-    </section>
-</>
+export default class Overview extends Component {
+    rate = (grade = 4) => {
+        let classificacao = []
+        for (let i = 0; i < 5; i++) {
+            if (grade > i) {
+                classificacao.push(<IoIosStar />)
+            } else {
+                classificacao.push(<IoIosStarOutline />)
+            }
+        }
+        return classificacao
+    }
 
-
-export default Overview;
+    render() {
+        return <>
+            <section className='card-container'>
+                <div className='card-item'>
+                    <div className="company-profile">
+                        <IoIosPeople />
+                    </div>
+                    <div className="company-data">
+                        <p>Nome Empresa</p>
+                        <p>example@company.org</p>
+                    </div>
+                    <div className="company-options">
+                        <p>{this.rate(4)}</p>
+                        <IoIosArrowForward />
+                    </div>
+                </div>
+                <div className='card-item'>
+                    <div className="company-profile">
+                        <IoIosPeople />
+                    </div>
+                    <div className="company-data">
+                        <p>Nome Empresa</p>
+                        <p>example@company.org</p>
+                    </div>
+                    <div className="company-options">
+                        <p>{this.rate(3)}</p>
+                        <IoIosArrowForward />
+                    </div>
+                </div>
+                <div className='card-item'>
+                    <div className="company-profile">
+                        <IoIosPeople />
+                    </div>
+                    <div className="company-data">
+                        <p>Nome Empresa</p>
+                        <p>example@company.org</p>
+                    </div>
+                    <div className="company-options">
+                        <p>{this.rate(3)}</p>
+                        <IoIosArrowForward />
+                    </div>
+                </div>
+                <div className='card-item'>
+                    <div className="company-profile">
+                        <IoIosPeople />
+                    </div>
+                    <div className="company-data">
+                        <p>Nome Empresa</p>
+                        <p>example@company.org</p>
+                    </div>
+                    <div className="company-options">
+                        <p>{this.rate(2)}</p>
+                        <IoIosArrowForward />
+                    </div>
+                </div>
+                <div className='card-item'>
+                    <div className="company-profile">
+                        <IoIosPeople />
+                    </div>
+                    <div className="company-data">
+                        <p>Nome Empresa</p>
+                        <p>example@company.org</p>
+                    </div>
+                    <div className="company-options">
+                        <p>{this.rate(0)}</p>
+                        <IoIosArrowForward />
+                    </div>
+                </div>
+            </section>
+        </>
+    }
+} 
